6bb66098fc6df25e96ce7e41594235d1b1982e6e
홍혁준 <jjj8337721@naver.com>
2023-05-08 22:37:15 +0900

    feat : DP

---
+++ hhj/src/java_algo_stduy/dp/계단오르기.java
@@ -0,0 +1,20 @@
+package src.java_algo_stduy.dp;
+
+public class 계단오르기 {
+    static int input = 7;
+    static int output = 21;
+
+    static int[] dy = new int[input+1];
+
+    public static void main(String[] args) {
+
+        dy[1] = 1;
+        dy[2] = 2;
+
+        for(int i=3; i<=input; i++){
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+
+        System.out.println(dy[input]);
+    }
+}
