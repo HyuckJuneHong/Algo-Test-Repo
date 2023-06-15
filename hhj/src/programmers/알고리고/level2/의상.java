dd4923d0eb6249887944d80805ae2947df321c0b
HyuckJuneHong <jjj8337721@naver.com>
2023-06-15 11:26:01 +0900

    feat : 6/13과 6/15

---
+++ hhj/src/programmers/알고리고/level2/의상.java
@@ -0,0 +1,29 @@
+package src.programmers.알고리고.level2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 의상 {
+
+    /**
+     * clothes의 각 행은 의상 이름, 의상 종류로 이루어짐
+     *
+     * @param clothes : 스파이가 가진 의상들
+     * @return : 서로 다른 조합수
+     */
+    public static int solution(String[][] clothes) {
+        Map<String, Integer> clothesMap = new HashMap<>();
+
+        for (String[] clothe : clothes) {
+            clothesMap.put(clothe[1], clothesMap.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        int answer = 1;
+
+        for (int clothe : clothesMap.values()) {
+            answer *= clothe + 1;
+        }
+
+        return answer - 1;
+    }
+}
