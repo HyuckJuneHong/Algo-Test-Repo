e53c53da738b61f363ccb306edf4d2ecf3c884d6
HyuckJuneHong <jjj8337721@naver.com>
2023-08-09 20:54:56 +0900

    feat : 8/9

--- hhj/src/Solution.java
+++ hhj/src/Solution.java
@@ -1,35 +1,9 @@
 package src;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class Solution {
-
-    // 1_000_000 * 1_000_000 -> 불가능
-    // DP로 풀어보자! -> 실패.. 시간초과...
-    // Stack? -> 성공
-    public static int[] solution(int[] numbers) {
-        Deque<Integer> index = new ArrayDeque<>();
-        index.push(0);
-
-        for (int i = 1; i < numbers.length; i++) {
-
-            while (!index.isEmpty() && numbers[index.peek()] < numbers[i]) {
-                numbers[index.pop()] = numbers[i];
-            }
-
-            index.push(i);
-        }
-
-        while (!index.isEmpty()) {
-            numbers[index.pop()] = -1;
-        }
-
-        return numbers;
-    }
 
     public static void main(String[] args) {
         int[] arr = {9, 1, 5, 3, 6, 2};
-        solution(arr);
+        Long.hashCode(1L);
     }
 }
