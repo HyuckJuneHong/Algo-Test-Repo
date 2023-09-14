87baf9cf93d65cb8000e9d2d10e203cd1e3d3fdb
HyuckJuneHong <jjj8337721@naver.com>
2023-09-14 09:09:04 +0900

    feat : 9/14

---
+++ hhj/src/알고리고/level2/마법의_엘리베이터.java
@@ -0,0 +1,24 @@
+package src.알고리고.level2;
+
+public class 마법의_엘리베이터 {
+	public int solution(int storey) {
+		int answer = dfs(storey, 0);
+
+		return answer;
+	}
+
+	private int dfs(int storey, int count) {
+		if (storey == 0) {
+			return count;
+		}
+
+		int x = storey % 10;
+
+		if (x > 5 || x == 5 && (storey / 10) % 10 >= 5) {
+			x = 10 - x;
+			storey += 10;
+		}
+
+		return dfs(storey / 10, count + x);
+	}
+}
