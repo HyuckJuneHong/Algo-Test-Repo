2be330fcb4713cf5c73f3b0fe2c783a3e65bb93f
HyuckJuneHong <jjj8337721@naver.com>
2023-09-25 11:22:02 +0900

    feat : 9/25

--- hhj/src/프로그래머스_코딩_테스트_문제_풀이_전략/_6장/카펫.java
+++ hhj/src/프로그래머스_코딩_테스트_문제_풀이_전략/_6장/카펫.java
@@ -2,17 +2,17 @@ package src.프로그래머스_코딩_테스트_문제_풀이_전략._6장;
 
 public class 카펫 {
     public int[] solution(int brown, int yellow) {
-        for (int i = 3; i <= 5000; i++) {
-            for (int j = 3; j <= i; j++) {
-                int b = (i + j - 2) * 2;
-                int y = i * j - b;
+        for (int w = 3; w <= 5000; w++) {
+            for (int h = 3; h <= w; h++) {
+                int b = 2 * (w + h - 2);
+                int y = w * h - b;
 
-                if (brown == b && yellow == y) {
-                    return new int[]{i, j};
+                if (b == brown && y == yellow) {
+                    return new int[]{w, h};
                 }
             }
         }
 
-        return null;
+        return new int[0];
     }
 }
