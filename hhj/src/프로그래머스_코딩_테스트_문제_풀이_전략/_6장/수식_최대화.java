e10a945206032bf30a354fcb95365447257e62de
HyuckJuneHong <jjj8337721@naver.com>
2023-09-26 11:34:45 +0900

    feat : 9/26

--- hhj/src/프로그래머스_코딩_테스트_문제_풀이_전략/_6장/수식_최대화.java
+++ hhj/src/프로그래머스_코딩_테스트_문제_풀이_전략/_6장/수식_최대화.java
@@ -1,24 +1,10 @@
-package src.알고리고.level2;
+package src.프로그래머스_코딩_테스트_문제_풀이_전략._6장;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-/**
- * 문제
- * - 주어진 +, -, * 연산자에 우선순위를 줘서 가장 큰 금액 찾기 (절대값)
- * <p>
- * 제한사항
- * - expression : 3~100
- * <p>
- * 풀이
- * 1. 연산자 우선순위를 미리 만들어보자.
- * 1. 연산자를 포함해 분리하자.
- * 2. 분리한 필드를 리스트에 담자.
- * 3. 정해진 우선순위에 따라 해당 식을 계산한 값으로 교체하자. (복사본 리스트가 있어야할 것 같음)
- * 4. 그리고 우선순위에 따라 계산한 값을 비교하자.
- */
-public class 수식최대화 {
+public class 수식_최대화 {
 
     static final String[][] PRIORITY = {
             {"*", "+", "-"},
