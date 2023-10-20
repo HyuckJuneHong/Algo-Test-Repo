f766055d61fbb1c569ca1b664ef97c0442eafbdd
HyuckJuneHong <jjj8337721@naver.com>
2023-10-20 10:29:20 +0900

    feat : 10/20

---
+++ hhj/src/알고리고/level2/숫자_블록.java
@@ -0,0 +1,36 @@
+package src.알고리고.level2;
+
+public class 숫자_블록 {
+
+    public int[] solution(long begin, long end) {
+        int[] answer = new int[(int) (end - begin + 1)];
+
+        for (long i = begin; i <= end; i++) {
+            if (i == 1) {
+                answer[(int) (i - begin)] = 0;
+                continue;
+            }
+
+            answer[(int) (i - begin)] = getMaxDiv(i);
+        }
+
+        return answer;
+    }
+
+    public int getMaxDiv(long n) {
+        double mid = Math.sqrt(n);
+        int result = 1;
+
+        for (long i = 2; i <= mid; i++) {
+            if (n % i == 0) {
+                if (n / i <= 10_000_000) {
+                    return (int) (n / i);
+                }
+
+                result = (int) i;
+            }
+        }
+
+        return result;
+    }
+}
