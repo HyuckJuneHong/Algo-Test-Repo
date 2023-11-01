7c7e5833c66980d977f7e4af7545882cb2ae13c6
HyuckJuneHong <jjj8337721@naver.com>
2023-11-01 10:53:20 +0900

    feat : 11/1

---
+++ hhj/src/알고리고/문제_풀이_전략/_2_문자열/lv1_시저_암호.java
@@ -0,0 +1,24 @@
+package src.알고리고.문제_풀이_전략._2_문자열;
+
+public class lv1_시저_암호 {
+    public String solution(String s, int n) {
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+
+            if (c == ' ') {
+                continue;
+            }
+
+            if (c >= 'a' && c <= 'z') {
+                arr[i] = (char) ((c - 'a' + n) % (26) + 'a');
+                continue;
+            }
+
+            arr[i] = (char) ((c - 'A' + n) % (26) + 'A');
+        }
+
+        return new String(arr);
+    }
+}
