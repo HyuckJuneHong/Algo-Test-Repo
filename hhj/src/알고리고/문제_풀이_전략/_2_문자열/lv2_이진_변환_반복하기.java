45bafaf30c9725a2f4484a25324fab54df46d252
HyuckJuneHong <jjj8337721@naver.com>
2023-11-06 01:36:21 +0900

    feat : 11/6

---
+++ hhj/src/알고리고/문제_풀이_전략/_2_문자열/lv2_이진_변환_반복하기.java
@@ -0,0 +1,35 @@
+package src.알고리고.문제_풀이_전략._2_문자열;
+
+/**
+ 문제 풀이
+ 1. 이진 변환을 하는 메서드 생성
+ 2. 해당 메서드를 s가 "1"이 될 때까지 반복
+ */
+public class lv2_이진_변환_반복하기 {
+
+	static int zeroCount = 0;
+	static int convertCount = 0;
+
+	public int[] solution(String s) {
+		while (!s.equals("1")) {
+			s = convert(s);
+			convertCount++;
+		}
+
+		return new int[] {convertCount, zeroCount};
+	}
+
+	public String convert(String s) {
+		int count = 0;
+
+		for (char c : s.toCharArray()) {
+			if (c == '1') {
+				count++;
+			} else {
+				zeroCount++;
+			}
+		}
+
+		return Integer.toString(count, 2);
+	}
+}
