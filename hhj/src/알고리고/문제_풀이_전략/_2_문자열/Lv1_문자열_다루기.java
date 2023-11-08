adab37340fcff4d6321c62b7514db2081253abd6
HyuckJuneHong <jjj8337721@naver.com>
2023-11-08 14:40:41 +0900

    feat: 11/8

---
+++ hhj/src/알고리고/문제_풀이_전략/_2_문자열/Lv1_문자열_다루기.java
@@ -0,0 +1,17 @@
+package src.알고리고.문제_풀이_전략._2_문자열;
+
+public class Lv1_문자열_다루기 {
+
+	public boolean solution(String s) {
+		if (s.length() != 4 && s.length() != 6) {
+			return false;
+		}
+
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+}
