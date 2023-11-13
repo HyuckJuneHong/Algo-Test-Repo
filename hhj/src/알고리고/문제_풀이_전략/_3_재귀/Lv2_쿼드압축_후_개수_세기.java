e070ea320065efae25686795267cdff6f2205293
HyuckJuneHong <jjj8337721@naver.com>
2023-11-13 22:44:21 +0900

    feat: 11/9

---
+++ hhj/src/알고리고/문제_풀이_전략/_3_재귀/Lv2_쿼드압축_후_개수_세기.java
@@ -0,0 +1,46 @@
+package src.알고리고.문제_풀이_전략._3_재귀;
+
+public class Lv2_쿼드압축_후_개수_세기 {
+
+	static int one = 0;
+	static int zero = 0;
+
+	public int[] solution(int[][] arr) {
+		dfs(arr, 0, 0, arr.length);
+
+		return new int[] {zero, one};
+	}
+
+	public void dfs(int[][] arr, int y, int x, int current) {
+		if (check(arr, y, x, current)) {
+			if (arr[y][x] == 1) {
+				one++;
+			} else {
+				zero++;
+			}
+
+			return;
+		}
+
+		int next = current / 2;
+
+		dfs(arr, y, x, next);
+		dfs(arr, y, x + next, next);
+		dfs(arr, y + next, x, next);
+		dfs(arr, y + next, x + next, next);
+	}
+
+	public boolean check(int[][] arr, int y, int x, int current) {
+		int value = arr[y][x];
+
+		for (int i = y; i < current + y; i++) {
+			for (int j = x; j < current + x; j++) {
+				if (arr[i][j] != value) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+}
