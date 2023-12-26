a3d4ac0121574cf6fb1e3464ffdf40dd8deea720
HyuckJuneHong <jjj8337721@naver.com>
2023-12-26 09:57:06 +0900

    feat: 12/26

---
+++ hhj/src/알고리고/문제_풀이_전략/_9_스택/Lv2_올바른_괄호_스택.java
@@ -0,0 +1,26 @@
+package src.알고리고.문제_풀이_전략._9_스택;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Lv2_올바른_괄호_스택 {
+
+	boolean solution(String s) {
+		Deque<Integer> stack = new ArrayDeque<>();
+
+		for (char c : s.toCharArray()) {
+			if (c == '(') {
+				stack.push(1);
+				continue;
+			}
+
+			if (stack.isEmpty()) {
+				return false;
+			}
+
+			stack.pop();
+		}
+
+		return stack.isEmpty();
+	}
+}
