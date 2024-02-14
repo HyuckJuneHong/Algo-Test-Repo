094a07b09b407c4f2e0d804ceff1ad31922fbd1f
HyuckJuneHong <jjj8337721@naver.com>
2024-02-14 15:47:03 +0900

    feat: 2/14

---
+++ hhj/src/알고리고/백준/Main.java
@@ -0,0 +1,24 @@
+package src.알고리고.백준;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		System.out.println(solution());
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+	private static int solution() {
+		return 0;
+	}
+}
